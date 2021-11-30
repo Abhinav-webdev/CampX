12fd36f7aebe1d3106f46f7fac2d675493511d77
AbhinavWebDev <abhinavwebdev@gmail.com>
2021-11-30 12:42:39 +0530

    First Commit

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
+app.listen(process.env.Port || 3000, () => {
     console.log('serving on port 3000')
 })
